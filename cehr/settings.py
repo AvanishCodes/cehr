1f8426b318c256953d365cfe1b093c6549311062
Avanish Gupta <avanishcodes@gmail.com>
2023-02-27 16:54:25 +0530

    Add secret to env file

--- cehr/settings.py
+++ cehr/settings.py
@@ -12,15 +12,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3vb-f!qm%o((z&q3129d=+a*!o_lwvoal#fq^*0ucgwkfx8730'
+SECRET_KEY = env('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
