7d4e00e5e98eb48c29e38a27de5a2a0b353cf01d
Avanish Gupta <avanishcodes@gmail.com>
2023-02-27 17:10:57 +0530

    Add serializers to models of address module

--- address/serializers/__init__.py
+++ address/serializers/__init__.py
@@ -1,3 +1,12 @@
-__all__ = (
+from .address import AddressSerializer
+from .city import CitySerializer
+from .country import CountrySerializer
+from .state import StateSerializer
 
+
+__all__ = (
+  AddressSerializer,
+  CitySerializer,
+  CountrySerializer,
+  StateSerializer,
 )
