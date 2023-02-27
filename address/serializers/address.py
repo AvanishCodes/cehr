0bd9c8afc2800f7a16bf598607f3987fbb070154
Avanish Gupta <avanishcodes@gmail.com>
2023-02-27 18:05:21 +0530

    Migration changes

--- address/serializers/address.py
+++ address/serializers/address.py
@@ -5,5 +5,5 @@ from ..models import Address
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ('id', 'al1', 'al2', 'city', 'state', 'pincode', 'country')
+        fields = ('id', 'al1', 'al2', 'city', 'state', 'pincode')
         read_only_fields = ('id',)
