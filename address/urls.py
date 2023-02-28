ad6300e9e3597a2e5cb5515cf300bba6a114bbca
Avanish Gupta <avanishcodes@gmail.com>
2023-02-28 16:06:39 +0530

    Create supporting modules to create an address

--- address/urls.py
+++ address/urls.py
@@ -15,6 +15,15 @@ urlpatterns = [
     # Address Model generics
     path('', AddressCreateList.as_view()),
     path('<int:pk>/', AddressRetrieveUpdateDestroy.as_view()),
+    
+    path('country/', CountryCreateList.as_view()),
+    path('country/<int:pk>/', CountryRetrieveUpdateDestroy.as_view()),
+
+    path('state/', StateCreateList.as_view()),
+    path('state/<int:pk>/', StateRetrieveUpdateDestroy.as_view()),
+
+    path('city/', CityCreateList.as_view()),
+    path('city/<int:pk>/', CityRetrieveUpdateDestroy.as_view()),
 ]
 
 # Export the urls
