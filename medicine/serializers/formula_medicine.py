06935d8061b45bf333a28f7d2f2a150b924d8b3b
Avanish Gupta <avanishcodes@gmail.com>
2023-03-11 11:19:32 +0530

    formula medicine model change effects

--- medicine/serializers/formula_medicine.py
+++ medicine/serializers/formula_medicine.py
@@ -1,13 +1,13 @@
 # medicine/serializers/formula_medicine.py
 from rest_framework import serializers
 
-from ..models import FormulaMedicine
+from ..models import Formula
 
 
 class FormulaMedicineSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = FormulaMedicine
+        model = Formula
         fields = ('id', 'shorthand', 'name', 'type', 'description',)
         read_only_fields = ('id',)
 
--- medicine/views/formula_medicine.py
+++ medicine/views/formula_medicine.py
@@ -1,19 +1,19 @@
 # medicine/views/formula_medicine.py
 from rest_framework import generics
 
-from ..models import FormulaMedicine
+from ..models import Formula
 from ..serializers import FormulaMedicineSerializer
 from ..pagination import DefaultPagination
 
 
 class FormulaMedicineListCreate(generics.ListCreateAPIView):
-    queryset = FormulaMedicine.objects.all()
+    queryset = Formula.objects.all()
     serializer_class = FormulaMedicineSerializer
     pagination_class = DefaultPagination
 
 
 class FormulaMedicineDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = FormulaMedicine.objects.all()
+    queryset = Formula.objects.all()
     serializer_class = FormulaMedicineSerializer
 
 
