aabd9ac831a791bd0438cddc14d654044336b21d
Avanish Gupta <avanishcodes@gmail.com>
2023-05-06 11:27:39 +0530

    Fiel permissions issue

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Doctor, Qualification, Specialization, Association, DoctorGroup, Membership, Venue, Sitting
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Qualification)
+admin.site.register(Specialization)
+admin.site.register(Association)
+admin.site.register(DoctorGroup)
+admin.site.register(Membership)
+admin.site.register(Venue)
+admin.site.register(Sitting)
